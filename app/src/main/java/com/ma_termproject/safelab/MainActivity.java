5f0cdc2922f43d76ede03c4e79b18e4179ed15cb
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-02 14:22:45 +0900

    로그인 화면 자바

--- app/src/main/java/com/ma_termproject/safelab/MainActivity.java
+++ app/src/main/java/com/ma_termproject/safelab/MainActivity.java
@@ -1,14 +1,36 @@
-package com.ma_termproject.safelab;
+package com.example.myapplication909;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTitle("로그인");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button=findViewById(R.id.login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+            }
+        });
+
+        Button sig=findViewById(R.id.sig);
+        sig.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,MainActivity8.class);
+                startActivity(intent);
+            }
+        });
     }
 }
