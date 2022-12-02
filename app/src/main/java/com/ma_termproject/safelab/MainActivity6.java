ede648590de0e624ade6df3d527e3d2561f79494
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-02 14:26:51 +0900

    긴급상황 자바

---
+++ app/src/main/java/com/ma_termproject/safelab/MainActivity6.java
@@ -0,0 +1,15 @@
+package com.example.myapplication909;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("긴급상황");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main6);
+    }
+}
