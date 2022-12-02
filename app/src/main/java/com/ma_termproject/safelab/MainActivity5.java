47fdde75fdbaf8ceec120ebd8fda6a77cce01de2
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-02 14:26:15 +0900

    회원정보 수정 자바

---
+++ app/src/main/java/com/ma_termproject/safelab/MainActivity5.java
@@ -0,0 +1,15 @@
+package com.example.myapplication909;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTitle("회원정보 수정");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main5);
+    }
+}
