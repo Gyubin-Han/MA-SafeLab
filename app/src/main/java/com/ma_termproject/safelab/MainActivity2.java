b0671b9e09e9b83e641d9d639242a40fe5c09dcd
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-07 14:06:30 +0900

    로그인 후 safe lab 화면

--- app/src/main/java/com/ma_termproject/safelab/MainActivity2.java
+++ app/src/main/java/com/ma_termproject/safelab/MainActivity2.java
@@ -19,7 +19,7 @@ public class MainActivity2 extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity2.this,MainActivity3.class);
+                Intent intent=new Intent(MainActivity2.this,Namesearch.class);
                 startActivity(intent);
             }
         });
