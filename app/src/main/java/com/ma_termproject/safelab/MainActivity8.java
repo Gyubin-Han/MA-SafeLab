0a74fac228b1dea7817d9b067b4c67c3c2b2f75c
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-12 20:39:39 +0900

    회원 가입 자바

--- app/src/main/java/com/ma_termproject/safelab/MainActivity8.java
+++ app/src/main/java/com/ma_termproject/safelab/MainActivity8.java
@@ -6,23 +6,68 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity8 extends AppCompatActivity {
 
+    private EditText et_id, et_pw, et_name, et_email, et_depart;
+    private Button btn_register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTitle("회원가입");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
 
-        Button btn1=findViewById(R.id.btn1);
-        btn1.setOnClickListener(new View.OnClickListener() {
+        et_id = findViewById(R.id.et_id);
+        et_pw = findViewById(R.id.et_pw);
+        et_name = findViewById(R.id.et_name);
+        et_email = findViewById(R.id.et_email);
+        et_depart = findViewById(R.id.et_depart);
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(MainActivity8.this,MainActivity.class);
-                startActivity(intent);
+                String id = et_id.getText().toString();
+                String pw = et_pw.getText().toString();
+                String name = et_name.getText().toString();
+                String email = et_email.getText().toString();
+                String depart = et_depart.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            boolean success = jsonObject.getBoolean("success");
+                            if(success){
+                                Toast.makeText(getApplicationContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(MainActivity8.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                };
+
+                com.example.myapplication909.RegisterRequest registerRequest = new com.example.myapplication909.RegisterRequest(id, pw, name, email, depart, responseListener);
+                RequestQueue queue = Volley.newRequestQueue(MainActivity8.this);
+                queue.add(registerRequest);
             }
         });
     }
-
 }
