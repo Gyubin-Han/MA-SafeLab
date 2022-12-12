579b75b3117a15cc33d43d347cf3b72077bf7621
Gyubin Han <89185979+Gyubin-Han@users.noreply.github.com>
2022-12-13 02:27:14 +0900

    긴급상황 - 문자메시지 발송 방법 수정
    긴급상황 문자 메시지 발송 메소드를 호출하는 방법을 변경하였음.

--- app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
+++ app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
@@ -10,9 +10,10 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class EmergencyInActivity extends Activity {
-    public static void sendSMS(String dest, String msg){
+    public static void sendSMS(String dest, String[] msg){
         SmsManager smsManager=SmsManager.getDefault();
-        smsManager.sendTextMessage(dest,null,msg,null,null);
+        for(int i=0;i<msg.length;i++)
+            smsManager.sendTextMessage(dest,null,msg[i],null,null);
     }
 
     protected void onCreate(Bundle savedInstanceState){
@@ -24,13 +25,11 @@ public class EmergencyInActivity extends Activity {
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Log.i("SafeLab","긴급상황 발동 시간 : "+sdf.format(date));
 
-        String message="(자동 발송)\n1] 화학물질과 관련하여 긴급한 상황이 발생하였습니다.";
-        sendSMS("999",message);
-        message="2] 주소: 부산 강서구 녹산산단382로14번가길 1201-35, 공학1관 404호";
-        sendSMS("999",message);
-        message="3] 일시: "+sdf.format(date);
-        sendSMS("999",message);
-        message="4] 본 메시지는 자동발송 되었고, 세부 상황에 대해 이 번호로 전화 부탁드립니다.";
+        String message[]=new String[4];
+        message[0]="(자동 발송)\n1] 화학물질과 관련하여 긴급한 상황이 발생하였습니다.";
+        message[1]="2] 주소: 부산 강서구 녹산산단382로14번가길 1201-35, 공학1관 404호";
+        message[2]="3] 일시: "+sdf.format(date);
+        message[3]="4] 본 메시지는 자동발송 되었고, 세부 상황에 대해 이 번호로 전화 부탁드립니다.";
         sendSMS("999",message);
     }
 }
