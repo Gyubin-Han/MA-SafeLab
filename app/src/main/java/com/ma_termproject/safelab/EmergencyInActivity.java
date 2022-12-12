a5d734c947f58475e72bb737ee8917dc0a31a30c
Gyubin Han <89185979+Gyubin-Han@users.noreply.github.com>
2022-12-12 16:10:31 +0900

    긴급상황 - 문자메시지 발송 기능 추가
    긴급상황 발생 시, 자동으로 문자 메시지를 발송할 수 있도록, 자동으로 발송할 수 있는 메소드를 추가함.

--- app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
+++ app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
@@ -1,10 +1,17 @@
 package com.ma_termproject.safelab;
 
+import android.telephony.SmsManager;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
 public class EmergencyInActivity extends Activity {
+    public static void sendSMS(String dest, String msg){
+        SmsManager smsManager=SmsManager.getDefault();
+        smsManager.sendTextMessage(dest,null,msg,null,null);
+    }
+
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
