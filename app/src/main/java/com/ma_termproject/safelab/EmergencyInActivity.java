4d54cc04c45d5791ca8fc8c3bf881163c92d8d7a
Gyubin Han <89185979+Gyubin-Han@users.noreply.github.com>
2022-12-13 02:42:33 +0900

    긴급상황 - 긴급상황 발생 종료 버튼 이벤트 추가
    긴급상황 화면에 있는 종료 버튼을 눌렀을 때 종료되는 이벤트를 추가하였음.

--- app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
+++ app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
@@ -5,6 +5,8 @@ import android.telephony.SmsManager;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.Date;
 import java.text.SimpleDateFormat;
@@ -25,11 +27,19 @@ public class EmergencyInActivity extends Activity {
         SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
         Log.i("SafeLab","긴급상황 발동 시간 : "+sdf.format(date));
 
+        Button btn_emer_off=(Button)findViewById(R.id.BtnEmergencyOff);
+        btn_emer_off.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                finish();
+            }
+        });
+
         String message[]=new String[4];
         message[0]="(자동 발송)\n1] 화학물질과 관련하여 긴급한 상황이 발생하였습니다.";
         message[1]="2] 주소: 부산 강서구 녹산산단382로14번가길 1201-35, 공학1관 404호";
         message[2]="3] 일시: "+sdf.format(date);
         message[3]="4] 본 메시지는 자동발송 되었고, 세부 상황에 대해 이 번호로 전화 부탁드립니다.";
         sendSMS("999",message);
+
     }
 }
