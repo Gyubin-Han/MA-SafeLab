b80b0d8361c568d6020a73fb3bb03600f1c639a8
Gyubin Han <89185979+Gyubin-Han@users.noreply.github.com>
2022-12-13 02:18:18 +0900

    긴급상황 - 문자메시지 자동 발송 기능 구현
    긴급상황 발생 시, 지정된 형식의 문자 메시지를 자동으로 발송되도록 함.

--- app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
+++ app/src/main/java/com/ma_termproject/safelab/EmergencyInActivity.java
@@ -6,6 +6,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 public class EmergencyInActivity extends Activity {
     public static void sendSMS(String dest, String msg){
         SmsManager smsManager=SmsManager.getDefault();
@@ -16,5 +19,18 @@ public class EmergencyInActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emergency);
         Log.i("SafeLab","긴급상황 발동됨!");
+
+        Date date=new Date(System.currentTimeMillis());
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+        Log.i("SafeLab","긴급상황 발동 시간 : "+sdf.format(date));
+
+        String message="(자동 발송)\n1] 화학물질과 관련하여 긴급한 상황이 발생하였습니다.";
+        sendSMS("999",message);
+        message="2] 주소: 부산 강서구 녹산산단382로14번가길 1201-35, 공학1관 404호";
+        sendSMS("999",message);
+        message="3] 일시: "+sdf.format(date);
+        sendSMS("999",message);
+        message="4] 본 메시지는 자동발송 되었고, 세부 상황에 대해 이 번호로 전화 부탁드립니다.";
+        sendSMS("999",message);
     }
 }
