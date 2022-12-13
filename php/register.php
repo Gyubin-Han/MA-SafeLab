0ef063f697dec0d9ad9380c8dcfdafe9baca9a4b
wlgur313 <34999115+wlgur313@users.noreply.github.com>
2022-12-13 17:57:56 +0900

    회원가입 php

--- php/register.php
+++ php/register.php
@@ -8,16 +8,25 @@
     $email = isset($_POST["email"]) ? $_POST["email"] : "";
     $depart = isset($_POST["depart"]) ? $_POST["depart"] : "";
 
+    // Check if the id already exists in the database
+    $check_id_query = mysqli_query($con, "SELECT * FROM user WHERE id='$id'");
+    if (mysqli_num_rows($check_id_query) > 0) {
+        // The id already exists
+        $response = array();
+        $response["success"] = false;
+        echo json_encode($response);
+        return;
+    }
+
+    // If we reach this point, the id does not exist yet and we can insert the data
     $statement = mysqli_prepare($con, "INSERT INTO user VALUES (?,?,?,?,?)");
     mysqli_stmt_bind_param($statement, "sssss", $id, $pw, $name, $email, $depart);
     mysqli_stmt_execute($statement);
 
-    
     $response = array();
     $response["success"] = true;
 
     echo json_encode($response);
 
     mysqli_close($con);
-
 ?>
